6a13e301b8313de332c346846641f86ad06efcb8
Nerdmaster <info@swiss-cyber-defence.ch>
2022-08-19 18:02:52 +0700

    Volatility

--- Scripts/Automation-Volatility/volatility-guru.py
+++ Scripts/Automation-Volatility/volatility-guru.py
@@ -2,10 +2,9 @@
 
 import os
 import argparse
-import pprint
-from subprocess import Popen, PIPE
 import subprocess
-import sys
+import command
+
 
 print("""\
 
@@ -42,8 +41,8 @@ volScript = "volatility "
 
 
 parser = argparse.ArgumentParser(description='Volatility Guru - Automated volatility analysis.')
-parser.add_argument('-f', '--file', help='File path to memory dump')
-parser.add_argument('-i', '--install', action='store_true', help='Install Volatility 2.6.0 via docker')
+parser.add_argument('-i', '--install', action='store_true', help='Install Volatility 2.6.1 via docker')
+parser.add_argument('-f', '--file', help='File name of memory dump in (/home/kali/dumps)')
 args = parser.parse_args()
 
 
@@ -54,15 +53,51 @@ if args.install == True:
 if args.file:
 	print("---- Get Operation System ----")
 
+	response = command.run([volScript])
+	print(response.output)
+	print(response.exit)
+
+
+	# Define command and options wanted
+	command = volScript
+	options = "-h"
+	# Ask user for file name(s) - now it's safe from shell injection
+	#filename = input("Please introduce name(s) of file(s) of interest:\n")
+	# Create list with arguments for subprocess.check_output
+	args=[]
+	args.append(command)
+	args.append(options)
+	#for i in filename.split():
+	#	args.append(i)
+	# Run subprocess.check_output and save command output
+
+
+	# Run subprocess.run and save output object
+	output = subprocess.run(args,capture_output=True, shell=True)
+	print(output)
+
+
+
+
 	# profileOS = os.popen(volScript + ' -f ' + args.file + 'imageinfo')
 	# print("Profile of Operating System: " + profileOS)
 
 	profileOS = "Win7SP1x64"
 
 	print("---- Process ----")
-	cmd = volScript + '" -f ' + args.file + " --profile " + profileOS + ' pslist"'
+	cmd = volScript + ' -f ~/dumps/' + args.file + " --profile " + profileOS + ' pslist'
 	print(cmd)
-	pslist = os.popen(cmd).read()
+
+
+	#p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	#for line in p.stdout.readlines():
+	#	print(line),
+	#retval = p.wait()
+
+	os.system(cmd)
+
+
+	# pslist = os.popen(volScript).read()
 	print("pslist: " + pslist)
 
 
