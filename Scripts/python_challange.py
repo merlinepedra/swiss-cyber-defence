b3ba218da553396eb3b5b4ef8ddcd2b6723bebe2
SCD <info@swiss-cyber-defence.ch>
2022-10-28 18:13:23 +0200

    python challange

--- Scripts/python_challange.py
+++ Scripts/python_challange.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python3
 
 import os
-import binascii
-import sqlite3
-import random
-import string
+import sys
+import re
+import json
+from collections import Counter, OrderedDict
 
-dbFilename = "customerdata.db"
-originalTableName = "customers"
-anonymizedTableName = "anonymizedData"
+
+accessLogName = "access.log"
 
 if __name__ == '__main__':
+
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
+
+    try:
+        file = open(accessLogName)
+    except:
+        print("File not found: {}", accessLogName)
+
+
+    # get all ip matches
+    ips = []
+    for line in file:
+        result = re.match(r"^([0-9]{1,3}(?:\.[0-9]{1,3}){3}) -", line)
+        if result:
+            ips.append(result.group(1))
+
+    
+    # create ordered mapping of counts
+    orderedIps = OrderedDict(Counter(ips).most_common())
+
+    # write json file
+    with open("./results.json", "w") as jsonFile:
+        json.dump(orderedIps, jsonFile, indent=2)
+    
+    print("Successfully aggregated log and wrote result.json")
 
   
 
